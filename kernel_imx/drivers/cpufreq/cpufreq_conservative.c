33ba8364ca4f85478cf3f4123e6f63d3f6fd2eb3
Bowen Cui <bowen.cui@mail.mcgill.ca>
2023-09-10 20:59:32 -0400

    Make the conservative governor more aggressive

--- kernel_imx/drivers/cpufreq/cpufreq_conservative.c
+++ kernel_imx/drivers/cpufreq/cpufreq_conservative.c
@@ -31,9 +31,9 @@ struct cs_dbs_tuners {
 };
 
 /* Conservative governor macros */
-#define DEF_FREQUENCY_UP_THRESHOLD		(80)
-#define DEF_FREQUENCY_DOWN_THRESHOLD		(20)
-#define DEF_FREQUENCY_STEP			(5)
+#define DEF_FREQUENCY_UP_THRESHOLD		(88)
+#define DEF_FREQUENCY_DOWN_THRESHOLD		(68)
+#define DEF_FREQUENCY_STEP			(20)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(10)
 
